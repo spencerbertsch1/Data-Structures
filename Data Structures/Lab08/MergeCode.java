46e47b6dbb75c196685f971115fd25b20dfa4118
Spencer Bertsch <spencerbertsch99@gmail.com>
2018-04-28 00:04:28 -0400

    Add files via upload

---
+++ Data Structures/Lab08/MergeCode.java
@@ -0,0 +1,32 @@
+
+public class MergeCode {
+
+	
+	public static void merge(int [] a, int startIdx, int midIdx, int endIdx ){
+		
+		int[] b = zeros a.length;
+		
+		//cut a in half depending on int midIdx
+		//		while loop
+		//find smallest of first two values
+		//put that value into index 0 of b
+		//increment the index of the array that had the smallest value 
+		//      end loop
+		
+		// after one side is done loop terminates
+		// second loop that throws all remaining data into other array b 
+		
+	}
+	
+	
+	public static void main(String[] args) {
+	
+		int startIdx = 0;
+		int midIdx = 5; 
+		int endIdx = 10; 
+		int[] a = {100, 90, 30, 40, 50, 6, 15, 45, 3, 9, 7 }  ; 
+		
+
+	}
+
+}
